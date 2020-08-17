20ae8447063b1781f188b965af2020932f530530
bartas0215 <poz.bartosznowak@gmail.com>
2020-08-17 21:27:05 +0200

    Minor changes

--- Load_script.R
+++ Load_script.R
@@ -12,10 +12,26 @@ glimpse(sur_1)
 
 # Delete unnecessary data
 sur_1 <- sur_1 %>%
-  select(- OÅ›wiadczam..Å¼e.przeczytaÅ‚am..em.i.zrozumiaÅ‚am..em.formularz.informacyjnych.dla.osÃ³b.biorÄ…cych.udziaÅ‚.w.badaniu.pt...Pandemia.koronawirusa.w.Å›wiadomoÅ›ci.spoÅ‚ecznej...RozpoczynajÄ…c.badanie.wyraÅ¼am.dobrowolnie.zgodÄ™.na.udziaÅ‚.w.badaniu.i.jestem.Å›wiadoma.y..iÅ¼.w.kaÅ¼dej.chwili.mogÄ™.zrezygnowaÄ‡.z.udziaÅ‚u.w.nim.bez.podania.przyczyny..Zgodnie.z.RozporzÄ…dzeniem.Parlamentu.Europejskiego.i.Rady..UE..2016.679.z.dnia.27.kwietnia.2016.r...RODO..wyraÅ¼am.zgodÄ™.na.przetwarzanie.przez.dr.n..biol..MagdalenÄ™.Frydrychowicz..Cezarego.Miedziarka..Bartosza.Nowaka..Szymona.PeÅ‚czyÅ„skiego..Uniwersytet.Medyczny.im..Karola.Marcinkowskiego.w.Poznaniu..moich.danych.osobowych.w.zakresie.i.celu.niezbÄ™dnych.do.przeprowadzenia.powyÅ¼szego.badania.oraz.w.celach.archiwalnych.i.statystycznych..Posiadam.wiedzÄ™.o.dobrowolnoÅ›ci.podania.danych.i.przysÅ‚ugujÄ…cym.mi.prawie.dostÄ™pu.do.treÅ›ci.danych.dotyczÄ…cych.mnie..ich.poprawiania..modyfikacji.oraz.skorzystania.z.innych.uprawnieÅ„.wynikajÄ…cych.z.ww..Ustawy.)
-
+  select(- Oœwiadczam..¿e.przeczyta³am..em.i.zrozumia³am..em.formularz.informacyjnych.dla.osób.bior¹cych.udzia³.w.badaniu.pt...Pandemia.koronawirusa.w.œwiadomoœci.spo³ecznej...Rozpoczynaj¹c.badanie.wyra¿am.dobrowolnie.zgodê.na.udzia³.w.badaniu.i.jestem.œwiadoma.y..i¿.w.ka¿dej.chwili.mogê.zrezygnowaæ.z.udzia³u.w.nim.bez.podania.przyczyny..Zgodnie.z.Rozporz¹dzeniem.Parlamentu.Europejskiego.i.Rady..UE..2016.679.z.dnia.27.kwietnia.2016.r...RODO..wyra¿am.zgodê.na.przetwarzanie.przez.dr.n..biol..Magdalenê.Frydrychowicz..Cezarego.Miedziarka..Bartosza.Nowaka..Szymona.Pe³czyñskiego..Uniwersytet.Medyczny.im..Karola.Marcinkowskiego.w.Poznaniu..moich.danych.osobowych.w.zakresie.i.celu.niezbêdnych.do.przeprowadzenia.powy¿szego.badania.oraz.w.celach.archiwalnych.i.statystycznych..Posiadam.wiedzê.o.dobrowolnoœci.podania.danych.i.przys³uguj¹cym.mi.prawie.dostêpu.do.treœci.danych.dotycz¹cych.mnie..ich.poprawiania..modyfikacji.oraz.skorzystania.z.innych.uprawnieñ.wynikaj¹cych.z.ww..Ustawy.)
 sur_1 <- sur_1 %>%
   select(-Sygnatura.czasowa)
 
 # Count number of females and males
-count(sur_1,ProszÄ™.wskazaÄ‡.swojÄ….pÅ‚eÄ‡)
+count(sur_1,Proszê.wskazaæ.swoj¹.p³eæ )
+
+# Count education 
+count(sur_1,Proszê.wskazaæ.swoje.wykszta³cenie )
+
+# Count age
+age_1 <- count(sur_1,Proszê.wskazaæ.swój.wiek..liczba. )
+print(age_1, n=60)
+
+age_1 %>%
+arrange(desc(n))
+
+# Count size of city/village
+count(sur_1,Proszê.wskazaæ.wielkoœæ.miejscowoœci..w.której.spêdzi³.a.Pan.Pani.wiêkszoœæ.swojego.¿ycia  )
+
+# Count number of children
+count(sur_1,Czy.posiada.Pan.i.dzieci..)
+
