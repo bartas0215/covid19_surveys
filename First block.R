9325237571d13f2f91e1c1e803f8f042485ec997
bartas0215 <poz.bartosznowak@gmail.com>
2021-01-25 09:51:34 +0100

    Script optimalization

--- First block.R
+++ First block.R
@@ -8,93 +8,61 @@ glimpse(sur_1)
 
 names(sur_1) = gsub(pattern = "Jak.czêsto.Pani.Pana.zdaniem.poni¿szy.objaw.wystêpuje.w.trakcie.zaka¿enia.COVID.19...", replacement = "", x = names(sur_1))
 
+
+###Function
+edu_1 <- function(x,y){
+  
+  sur_3_3 <- sur_3_2 %>% 
+    group_by({{x}}) %>%
+    count(biegunka.)
+  
+  sur_3_4 <- sur_3_2 %>% 
+    group_by({{x}}) %>%
+    count(kaszel.)
+  
+  sur_3_5 <- sur_3_2 %>% 
+    group_by({{x}}) %>%
+    count(bóle.miêœni.)
+  
+  sur_3_6 <- sur_3_2 %>% 
+    group_by({{x}}) %>%
+    count(dusznoœæ.)
+  
+  sur_3_7 <- sur_3_2 %>% 
+    group_by({{x}}) %>%
+    count(zmêczenie.)
+  
+  sur_3_8 <- sur_3_2 %>% 
+    group_by({{x}}) %>%
+    count(zaburzenia.smaku.i.wêchu. )
+  
+  sur_3_9 <- sur_3_2 %>% 
+    group_by({{x}}) %>%
+    count(zapalenie.spojówek. )
+  
+  # Save data as one file
+  
+  list_of_datasets <- list("DataSheet" = sur_3_3, "Datasheet" = sur_3_4,"Datasheet" = sur_3_5,"Datasheet" = sur_3_6,"Datasheet" = sur_3_7,
+                           "Datasheet" = sur_3_8,"Datasheet" = sur_3_9)
+  write.xlsx(list_of_datasets, file = y)
+}
+
 ####Education (medical studies/non medical studies)
 
 sur_1_1 <- sur_1 %>%
   select( Proszê.wskazaæ.swoje.wykszta³cenie,biegunka.:zapalenie.spojówek.)
 
-sur_1_2 <- sur_1_1 %>%
+sur_3_2 <- sur_1_1 %>%
   filter(Proszê.wskazaæ.swoje.wykszta³cenie == "w trakcie studiów wy¿szych (niemedycznych)" |Proszê.wskazaæ.swoje.wykszta³cenie == "w trakcie studiów wy¿szych (medycznych)")
 
-glimpse(sur_1_2)
-
-# Counting symptomps
-sur_1_3 <- sur_1_2 %>% 
-  group_by(Proszê.wskazaæ.swoje.wykszta³cenie) %>%
-  count(biegunka.)
-
-sur_1_4 <- sur_1_2 %>% 
-  group_by(Proszê.wskazaæ.swoje.wykszta³cenie) %>%
-  count(kaszel.)
-
-sur_1_5 <- sur_1_2 %>% 
-  group_by(Proszê.wskazaæ.swoje.wykszta³cenie) %>%
-  count(bóle.miêœni.)
-
-sur_1_6 <- sur_1_2 %>% 
-  group_by(Proszê.wskazaæ.swoje.wykszta³cenie) %>%
-  count(dusznoœæ.)
-
-sur_1_7 <- sur_1_2 %>% 
-  group_by(Proszê.wskazaæ.swoje.wykszta³cenie) %>%
-  count(zmêczenie.)
-
-sur_1_8 <- sur_1_2 %>% 
-  group_by(Proszê.wskazaæ.swoje.wykszta³cenie) %>%
-  count(zaburzenia.smaku.i.wêchu. )
-
-sur_1_9 <- sur_1_2 %>% 
-  group_by(Proszê.wskazaæ.swoje.wykszta³cenie) %>%
-  count(zapalenie.spojówek. )
-
-
-# Save data as one file
-
-list_of_datasets <- list("DataSheet" = sur_1_3, "Datasheet" = sur_1_4,"Datasheet" = sur_1_5,"Datasheet" = sur_1_6,"Datasheet" = sur_1_7,
-                         "Datasheet" = sur_1_8,"Datasheet" = sur_1_9)
-write.xlsx(list_of_datasets, file = "C:/Users/Bartek/Desktop/Ankieta COVID/new_dem/symptomps_education/symptomps.xlsx")
+edu_1(x =sur_3_2$Proszê.wskazaæ.swoje.wykszta³cenie,"C:/Users/Bartek/Desktop/Ankieta COVID/new_dem/symptomps_education/symptomps.xlsx")
 
 #### Gender
 
-sur_2_1 <- sur_1 %>%
+sur_3_2 <- sur_1 %>%
   select( Proszê.wskazaæ.swoj¹.p³eæ ,biegunka.:zapalenie.spojówek.)
 
-# Counting symptomps
-sur_2_3 <- sur_2_1 %>% 
-  group_by(Proszê.wskazaæ.swoj¹.p³eæ) %>%
-  count(biegunka.)
-
-sur_2_4 <- sur_2_1 %>% 
-  group_by(Proszê.wskazaæ.swoj¹.p³eæ) %>%
-  count(kaszel.)
-
-sur_2_5 <- sur_2_1 %>% 
-  group_by(Proszê.wskazaæ.swoj¹.p³eæ) %>%
-  count(bóle.miêœni.)
-
-sur_2_6 <- sur_2_1 %>% 
-  group_by(Proszê.wskazaæ.swoj¹.p³eæ) %>%
-  count(dusznoœæ.)
-
-sur_2_7 <- sur_2_1 %>% 
-  group_by(Proszê.wskazaæ.swoj¹.p³eæ) %>%
-  count(zmêczenie.)
-
-sur_2_8 <- sur_2_1 %>% 
-  group_by(Proszê.wskazaæ.swoj¹.p³eæ) %>%
-  count(zaburzenia.smaku.i.wêchu. )
-
-sur_2_9 <- sur_2_1 %>% 
-  group_by(Proszê.wskazaæ.swoj¹.p³eæ) %>%
-  count(zapalenie.spojówek. )
-
-
-# Save data as one file
-
-list_of_datasets <- list("DataSheet" = sur_2_3, "Datasheet" = sur_2_4,"Datasheet" = sur_2_5,"Datasheet" = sur_2_6,"Datasheet" = sur_2_7,
-                         "Datasheet" = sur_2_8,"Datasheet" = sur_2_9)
-write.xlsx(list_of_datasets, file = "C:/Users/Bartek/Desktop/Ankieta COVID/new_dem/symptomps_gender/symptomps.xlsx")
-
+edu_1(x =sur_3_2$Proszê.wskazaæ.swoj¹.p³eæ,"C:/Users/Bartek/Desktop/Ankieta COVID/new_dem/symptomps_gender/symptomps.xlsx")
 
 ### Medical vs non-medical
 
@@ -104,44 +72,7 @@ sur_3_1 <- sur_1 %>%
 sur_3_2 <- sur_3_1 %>%
   filter(Proszê.wskazaæ.swoje.wykszta³cenie == "wy¿sze (medyczne)" |Proszê.wskazaæ.swoje.wykszta³cenie == "wy¿sze (niemedyczne)")
 
-glimpse(sur_3_2)
-
-# Counting symptomps
-sur_3_3 <- sur_3_2 %>% 
-  group_by(Proszê.wskazaæ.swoje.wykszta³cenie) %>%
-  count(biegunka.)
-
-sur_3_4 <- sur_3_2 %>% 
-  group_by(Proszê.wskazaæ.swoje.wykszta³cenie) %>%
-  count(kaszel.)
-
-sur_3_5 <- sur_3_2 %>% 
-  group_by(Proszê.wskazaæ.swoje.wykszta³cenie) %>%
-  count(bóle.miêœni.)
-
-sur_3_6 <- sur_3_2 %>% 
-  group_by(Proszê.wskazaæ.swoje.wykszta³cenie) %>%
-  count(dusznoœæ.)
-
-sur_3_7 <- sur_3_2 %>% 
-  group_by(Proszê.wskazaæ.swoje.wykszta³cenie) %>%
-  count(zmêczenie.)
-
-sur_3_8 <- sur_3_2 %>% 
-  group_by(Proszê.wskazaæ.swoje.wykszta³cenie) %>%
-  count(zaburzenia.smaku.i.wêchu. )
-
-sur_3_9 <- sur_3_2 %>% 
-  group_by(Proszê.wskazaæ.swoje.wykszta³cenie) %>%
-  count(zapalenie.spojówek. )
-
-
-# Save data as one file
-
-list_of_datasets <- list("DataSheet" = sur_3_3, "Datasheet" = sur_3_4,"Datasheet" = sur_3_5,"Datasheet" = sur_3_6,"Datasheet" = sur_3_7,
-                         "Datasheet" = sur_3_8,"Datasheet" = sur_3_9)
-write.xlsx(list_of_datasets, file = "C:/Users/Bartek/Desktop/Ankieta COVID/new_dem/symptomps_medical/symptomps.xlsx")
-
+edu_1(x =sur_3_2$Proszê.wskazaæ.swoje.wykszta³cenie,"C:/Users/Bartek/Desktop/Ankieta COVID/new_dem/symptomps_medical/symptomps.xlsx")
 
 ### Correlation_age
 
