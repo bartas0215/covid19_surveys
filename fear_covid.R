8aa05bf4eac2d8c08536f381101cfaeef5ed95cb
bartas0215 <poz.bartosznowak@gmail.com>
2021-02-04 23:46:45 +0100

    Plot generating function

--- fear_covid.R
+++ fear_covid.R
@@ -37,21 +37,33 @@ sur_4_2 <- sur_1 %>%
 sur_4_2 <- sur_4_2 %>%
   filter(!str_detect( Proszê.wskazaæ.swoj¹.p³eæ, fixed("inna")))
 
+# Function
+filter_fear <- function(x,y,z,q) {
+
 # Filter data
 sur_4_3 <- sur_4_2 %>%
   group_by(Proszê.wskazaæ.swoj¹.p³eæ) %>%
-  summarise(utrata.pracy.) %>%
-  count(utrata.pracy.)
-
+  summarise({{x}}) %>%
+  count({{x}})
+        
 # Add additional column
-sur_4_5 <- sur_4_3 %>% 
+sur_4_6 <- sur_4_3 %>% 
   mutate(Procent = ifelse(str_detect(Proszê.wskazaæ.swoj¹.p³eæ,fixed("mê¿czyzna")), (n/219)*100, (n/1161)*100))
 
+write.xlsx(sur_4_6, file = q)
+
 # Perform plot
-plot_fear <-ggplot(sur_4_2,aes(x =utrata.pracy.,y= Procent)) +
+plot_fear <-ggplot(sur_4_6,aes(x ={{x}},y= {{y}})) +
   geom_col() + facet_wrap(~Proszê.wskazaæ.swoj¹.p³eæ)
 
 # Save plot
-ggsave(plot =plot_fear,filename = "1.png", dpi = 600 , path = "C:/Users/Bartek/Desktop/Ankieta COVID/Plots/gender", device = "png")
+ggsave(plot =plot_fear,filename = z, dpi = 600 , path = "C:/Users/Bartek/Desktop/Ankieta COVID/Plots/gender", device = "png")
 
+}
 
+filter_fear(x =utrata.pracy., y = Procent, z = "1_gender.png", q="C:/Users/Bartek/Desktop/Ankieta COVID/new_dem/Fear_covid/gender/1")
+filter_fear(x =utrata.w³asnego.zdrowia., y = Procent, z = "2_gender.png",q="C:/Users/Bartek/Desktop/Ankieta COVID/new_dem/Fear_covid/gender/2")
+filter_fear(x =utrata.zdrowia.bliskiej.osoby., y = Procent, z = "3_gender.png",q="C:/Users/Bartek/Desktop/Ankieta COVID/new_dem/Fear_covid/gender/3")
+filter_fear(x =kryzys.gospodarczy., y = Procent, z = "4_gender.png",q="C:/Users/Bartek/Desktop/Ankieta COVID/new_dem/Fear_covid/gender/4")
+filter_fear(x =kryzys.polityczny., y = Procent, z = "5_gender.png",q="C:/Users/Bartek/Desktop/Ankieta COVID/new_dem/Fear_covid/gender/5")
+filter_fear(x =zwiêkszenie.kontroli.pañstwa.nad.obywatelami., y = Procent, z = "6_gender.png",q="C:/Users/Bartek/Desktop/Ankieta COVID/new_dem/Fear_covid/gender/6")
